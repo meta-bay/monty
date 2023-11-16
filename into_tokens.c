203dc0571b1f82f0175547e278e6a77106b1396c
meta-bay <bayiemeseret@gmail.com>
2023-11-17 02:05:31 +0300

    add into_tokens

---
+++ into_tokens.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * tokenize - break the line into tokens
+*/
+
+void tokenize(void)
+{
+	glob_vars.the_tokens[0] = strtok(glob_vars.line_read, " \t\n");
+	glob_vars.the_tokens[1] = strtok(NULL, "\t\n");
+	glob_vars.the_tokens[2] = NULL;
+}
